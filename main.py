f79e90a9fef0cc0048df397bd4fd5215a807c276
Martin Winkler <martin.winkler@relaxdays.de>
2023-06-26 13:49:49 +0200

    Balkendiagramm für gepickte positionen pro Cluster

--- main.py
+++ main.py
@@ -1,8 +1,64 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
+def display_picked_positions_by_year_and_month(dataframe):
+    picked_positions_by_year_and_month = dataframe.groupby(['Year', 'Month'])['PicklistenPositionenID'].count().reset_index()
+    picked_positions_by_year_and_month = picked_positions_by_year_and_month.rename(columns={'PicklistenPositionenID': 'PickedPositions'})
+    picked_positions_by_year_and_month['YearAndMonth'] = picked_positions_by_year_and_month['Year'].astype(str) + '-' + picked_positions_by_year_and_month['Month'].astype(str)
+
+    x = picked_positions_by_year_and_month['YearAndMonth'].to_list()
+    y = picked_positions_by_year_and_month['PickedPositions'].to_list()
+
+    plt.bar(x, y)
+    plt.xlabel("Monat und Jahr")
+    plt.ylabel("Gepickte Positionen")
+    plt.title("Gepickte Positionen pro Monat und Jahr")
+    plt.show()
+
+
+def display_picked_positions_by_year_and_month_and_cluster(dataframe):
+    picked_positions_by_year_and_month_and_cluster = dataframe.groupby(['Year', 'Month', 'Cluster'])['PicklistenPositionenID'].count().reset_index()
+    picked_positions_by_year_and_month_and_cluster = picked_positions_by_year_and_month_and_cluster.rename(columns={'PicklistenPositionenID': 'PickedPositions'})
+    picked_positions_by_year_and_month_and_cluster['YearAndMonth'] = picked_positions_by_year_and_month_and_cluster['Year'].astype(str) + '-' + picked_positions_by_year_and_month_and_cluster['Month'].astype(str)
+
+    months = picked_positions_by_year_and_month_and_cluster['YearAndMonth'].unique().tolist()
+
+    cluster_counts = {}
+    c1_values_dataframe = picked_positions_by_year_and_month_and_cluster[picked_positions_by_year_and_month_and_cluster['Cluster'] == 'C1']
+    cluster_counts['C1'] = np.array(c1_values_dataframe['PickedPositions'])
+
+    c1_values_dataframe = picked_positions_by_year_and_month_and_cluster[picked_positions_by_year_and_month_and_cluster['Cluster'] == 'C2']
+    cluster_counts['C2'] = np.array(c1_values_dataframe['PickedPositions'])
+
+    c1_values_dataframe = picked_positions_by_year_and_month_and_cluster[picked_positions_by_year_and_month_and_cluster['Cluster'] == 'C3']
+    cluster_counts['C3'] = np.array(c1_values_dataframe['PickedPositions'])
+
+    c1_values_dataframe = picked_positions_by_year_and_month_and_cluster[picked_positions_by_year_and_month_and_cluster['Cluster'] == 'OOR']
+    cluster_counts['OOR'] = np.array(c1_values_dataframe['PickedPositions'])
+
+    width = 0.6
+
+    fig, ax = plt.subplots()
+    bottom = np.zeros(len(months))
+
+    for cluster, cluster_count in cluster_counts.items():
+        p = ax.bar(months, cluster_count, width, label=cluster, bottom=bottom)
+        bottom += cluster_count
+        ax.bar_label(p, label_type='center')
+
+    ax.set_title("Number of Picked Positions by Year, Month and Cluster")
+    ax.legend(loc="upper center")
+
+    plt.show()
+
+
 dataframe_picklisten_raw = pd.read_csv('Picklisten.csv')
 dataframe_picklisten_positionen_raw = pd.read_csv('PicklistenPositionen.csv')
+
+# nur Großkugel betrachten, da letztes verbelibendes Lager
+dataframe_picklisten_raw = dataframe_picklisten_raw[dataframe_picklisten_raw['Standort'] == 'GRO']
 
 dataframe_picklisten = dataframe_picklisten_raw[['PicklistenID', 'Picker', 'Erstellungsdatum', 'Cluster', 'Standort', 'FinishedTime', 'inProcessTime', 'PufferschieneTime']]
 dataframe_picklisten_positionen = dataframe_picklisten_positionen_raw[['PicklistenPositionenID', 'PicklistenID', 'Menge', 'Picker', 'Pickzeit', 'Packer', 'Packzeit']]
@@ -20,16 +76,7 @@ joined_dataframe = pd.merge(dataframe_picklisten, dataframe_picklisten_positione
 
 joined_dataframe['Year'] = pd.DatetimeIndex(joined_dataframe['Erstellungsdatum']).year
 joined_dataframe['Month'] = pd.DatetimeIndex(joined_dataframe['Erstellungsdatum']).month
+joined_dataframe['Day'] = pd.DatetimeIndex(joined_dataframe['Erstellungsdatum']).day
 
-picked_positions_by_year_and_month = joined_dataframe.groupby(['Year', 'Month'])['PicklistenPositionenID'].count().reset_index()
-picked_positions_by_year_and_month = picked_positions_by_year_and_month.rename(columns={'PicklistenPositionenID': 'PickedPositions'})
-picked_positions_by_year_and_month['YearAndMonth'] = picked_positions_by_year_and_month['Year'].astype(str) + '-' + picked_positions_by_year_and_month['Month'].astype(str)
-
-x = picked_positions_by_year_and_month['YearAndMonth'].to_list()
-y = picked_positions_by_year_and_month['PickedPositions'].to_list()
-
-plt.bar(x, y)
-plt.xlabel("Monat und Jahr")
-plt.ylabel("Gepickte Positionen")
-plt.title("Gepickte Positionen pro Monat und Jahr")
-plt.show()
+# display_picked_positions_by_year_and_month(joined_dataframe)
+display_picked_positions_by_year_and_month_and_cluster(joined_dataframe)
